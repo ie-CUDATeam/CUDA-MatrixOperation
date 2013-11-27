5670a283e70a794020acc2a1f1e388c7a78769da
Kouta TOUME <k138567@ie.u-ryukyu.ac.jp>
2013-11-27 21:36:17 +0900

    簡単なCUDAプログラムを追加.

---
+++ Makefile
@@ -0,0 +1,9 @@
+NVCC	= nvcc
+INCLUDE	= -I ~/NVIDIA_CUDA-5.5_Samples/common/inc
+TARGETS	= hello_world
+
+hello_world: hello_world.cu
+	$(NVCC) $(INCLUDE) $< -o $@
+
+clean:
+	$(RM) $(TARGETS) a.out *~
---
+++ hello_world.cu
@@ -0,0 +1,20 @@
+#include <iostream>
+
+#include <cuda_runtime.h>
+#include <helper_functions.h>
+#include <helper_cuda.h>
+
+
+int main(int argc, char *argv[])
+{
+    using namespace std;
+
+    int devId = findCudaDevice(argc, (const char **)argv);
+    cout << "devId = " << devId << endl;
+
+    cudaDeviceProp prop;
+    cudaGetDeviceProperties(&prop, devId);
+    cout << "multiProcessorCount = " << prop.multiProcessorCount << endl;
+
+    return 0;
+}
